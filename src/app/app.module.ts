5018d8b8f6afe6e01e3cc7a4aa4b1b46888d5e1b
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2017-12-12 07:55:50 +0100

    Use Service Workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {ServiceWorkerModule} from '@angular/service-worker';
 import 'hammerjs';
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
+import {AppComponent} from './app.component';
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
+import {environment} from '../environments/environment';
 
 const routes: Routes = [];
 
@@ -22,8 +24,10 @@ const routes: Routes = [];
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     SharedModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
