40d1710455eff38bc27771707709db54f614cf88
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2018-04-26 12:12:28 +0200

    Dev environment and Github pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  apiEndpoint: 'https://lafeuille-ljcp-api.herokuapp.com',
+  apiEndpoint: 'https://lafeuille-ljcp-api-dev.herokuapp.com',
   production: false
 };
