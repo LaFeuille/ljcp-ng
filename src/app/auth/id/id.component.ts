c808cb94850554b50dd013cd0460c793d8059678
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2019-09-23 21:15:15 +0200

    Upgrade dependencies and Implement reconnection

--- src/app/auth/id/id.component.ts
+++ src/app/auth/id/id.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { AuthQuery } from '../state';
+import { Component, OnInit } from '@angular/core';
+import { AuthQuery, AuthService } from '../state';
 
 @Component({
   templateUrl: './id.component.html'
 })
-export class IdComponent {
+export class IdComponent implements OnInit {
 
-  constructor(public query: AuthQuery) {
+  constructor(public query: AuthQuery, private service: AuthService) {
+    this.service.renewTokens();
+  }
+
+  ngOnInit(): void {
+  }
+
+  onLogOut() {
+    this.service.logout();
   }
 
 }
